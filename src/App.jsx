84f24d0e9691ea6ca298d57dec850e09f6ab7b5d
21dagi <21dagmawinegussie@gmail.com>
2025-05-07 21:38:01 +0300

    structured folders and edited home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar.component';
-import UserAuthForm from './pages/userAuthForm.page';
-import Editor from './pages/editor.pages'; // Import your Editor component
-import { createContext } from 'react';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import UserAuthForm from "./pages/userAuthForm.page";
+import Editor from "./pages/editor.pages"; // Import your Editor component
+import { createContext } from "react";
+import Home from "./pages/dashboard/Home";
+import Wrapper from "./components/Wrapper";
+import ViewBlog from "./pages/blog/ViewBlog";
+import EditBlog from "./pages/blog/EditBlog";
+import Reports from "./pages/blog/Reports";
 
 //export const UserContext = createContext({})
 
@@ -12,13 +17,18 @@ const App = () => {
       let userInSession = lookInSesstion("user");
       userInSession ? setUserAuth(JSON.parse(userInSession)) : setUserAuth({access_token: null});
   }, []) */
-  
+
   return (
     //<UserContext.Provider value={userAuth, setUserAuth}>
     <Routes>
-      <Route path="/" element={<Navbar />}>
+      <Route element={<Wrapper />}>
+        <Route path="/" element={<Home />} />
+
         <Route path="signin" element={<UserAuthForm type="sign-in" />} />
         <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+        <Route path="blogs/edit/:id" element={<EditBlog />} />
+        <Route path="blogs/:id" element={<ViewBlog />} />
+        <Route path="reports" element={<Reports />} />
       </Route>
       <Route path="/editor" element={<Editor />} />
     </Routes>
