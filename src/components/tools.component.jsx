d1e9c791072e95027291d88968d7d75ac1d0e46e
21dagi <21dagmawinegussie@gmail.com>
2025-05-09 09:28:14 +0300

    gitignore

--- src/components/tools.component.jsx
+++ src/components/tools.component.jsx
@@ -1,10 +1,10 @@
-import Embed from "@editorjs/embed";
-import List from "@editorjs/list";
-import Image from "@editorjs/image";
-import Header from "@editorjs/header";
-import Quote from "@editorjs/quote";
-import Marker from "@editorjs/marker"; // Corrected the import name
-import InlineCode from "@editorjs/inline-code";
+import Embed from '@editorjs/embed';
+import List from '@editorjs/list';
+import Image from '@editorjs/image';
+import Header from '@editorjs/header';
+import Quote from '@editorjs/quote';
+import Marker from '@editorjs/marker'; // Corrected the import name
+import InlineCode from '@editorjs/inline-code';
 // import {uploadImage} from "../common/aws";
 
 /*const uploadImageByFile = (e) =>{
@@ -36,34 +36,33 @@ const uploadImageByURL = (e) =>{
     })
 }*/
 
-
 export const tools = {
-    embed: Embed,
-    list: {
-        class: List,
-        inlineToolbar: true
+  embed: Embed,
+  list: {
+    class: List,
+    inlineToolbar: true,
+  },
+  image: {
+    class: Image,
+    config: {
+      uploader: {
+        // uploadByUrl: uploadImageByURL,
+        // uploadByFile : uploadImagByFile
+      },
     },
-    image: {
-        class: Image,
-        config: {
-            uploader: {
-                // uploadByUrl: uploadImageByURL, 
-                // uploadByFile : uploadImagByFile
-            }
-        }
+  },
+  header: {
+    class: Header,
+    config: {
+      placeholder: 'Type Heading...',
+      levels: [2, 3],
+      defaultLevel: 2,
     },
-    header: {
-        class: Header,
-        config: {
-            placeholder: "Type Heading...",
-            levels: [2, 3],
-            defaultLevel: 2
-        }
-    },
-    quote: {
-        class: Quote,
-        inlineToolbar: true
-    },
-    marker: Marker, // Added missing comma here
-    inlineCode: InlineCode
+  },
+  quote: {
+    class: Quote,
+    inlineToolbar: true,
+  },
+  marker: Marker, // Added missing comma here
+  inlineCode: InlineCode,
 };
--- src/pages/editor.pages.jsx
+++ src/pages/editor.pages.jsx
@@ -1,9 +1,9 @@
-import { Navigate } from "react-router-dom";
-import { useContext, useState } from "react";
+import { Navigate } from 'react-router-dom';
+import { useContext, useState } from 'react';
 //import { useContext} from "../App";
-import BlogEditor from "../components/blog-editor.component";
-import PublishForm from  "../components/publish-form.component";
-import { createContext } from "react";
+import BlogEditor from '../components/blog-editor.component';
+import PublishForm from '../components/publish-form.component';
+import { createContext } from 'react';
 
 /*const blogStructure = {
 
@@ -17,22 +17,21 @@ import { createContext } from "react";
 
 //export const EditorContext = createContext({});
 
-const Editor = () =>{
-     // const [blog, setBlog ] = useState(blogStructure)
-     // const [ editorState, setEditorState] = useState("editor");
-     // const [textEditor, setTextEditor ] = useState({isReady: false});
-     // let {userAuth: {access_token}} = useContext(UserContext)
-    return(
-      //<EditorContext.Provider  value = {{ blog,setBlog, editorState,setEditorState, textEditor, setTextEditor}}
-      // user can only edit when its logged in so here is the condition
-      /* {access_token === null ? <Navigate to="/signin"/>
+const Editor = () => {
+  // const [blog, setBlog ] = useState(blogStructure)
+  // const [ editorState, setEditorState] = useState("editor");
+  // const [textEditor, setTextEditor ] = useState({isReady: false});
+  // let {userAuth: {access_token}} = useContext(UserContext)
+  return (
+    //<EditorContext.Provider  value = {{ blog,setBlog, editorState,setEditorState, textEditor, setTextEditor}}
+    // user can only edit when its logged in so here is the condition
+    /* {access_token === null ? <Navigate to="/signin"/>
        :editorState == "editor" ? <BlogEditor/> : <PublishForm/>
        } */
-       <>
-       
-       <BlogEditor />
-       <PublishForm />
-   </>
-    )
-}
+    <>
+      <BlogEditor />
+      <PublishForm />
+    </>
+  );
+};
 export default Editor;
