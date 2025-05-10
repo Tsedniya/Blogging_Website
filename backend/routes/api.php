4fef8ffe38e0a57159aeb6d68dc82e80619fbb2b
21dagi <21dagmawinegussie@gmail.com>
2025-05-10 16:44:37 +0300

    Implement comment addition and reporting functionality; enhance blog view with user profile and follow/unfollow features

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -29,6 +29,8 @@ Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallbac
 
 Route::get('/index', [BlogController::class, 'index']);
 
-    Route::get('/auth/check', [BlogController::class, 'checkAuth']);
-    Route::get('/blogs/{id}', [BlogController::class, 'getBlog']);
-    Route::get('/blogs/{id}/comments', [BlogController::class, 'getComments']);
+Route::get('/auth/check', [BlogController::class, 'checkAuth']);
+Route::get('/blogs/{id}', [BlogController::class, 'getBlog']);
+Route::get('/blogs/{id}/comments', [BlogController::class, 'getComments']);
+Route::post('/blogs/{id}/comments', [BlogController::class, 'addComment']);
+Route::post('/blogs/{id}/report', [BlogController::class, 'reportBlog']);
