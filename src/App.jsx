7f8e801f8be969dbdbc579fa70b8d6bca9e98fa4
Betelhem Tekle <111254433+bettyberry@users.noreply.github.com>
2025-05-13 11:45:40 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar.component';
 import UserAuthForm from './pages/userAuthForm.page';
 import Editor from './pages/editor.pages'; // Import your Editor component
-import { createContext } from 'react';
 import Home from './pages/dashboard/Home';
 import Wrapper from './components/Wrapper';
 import ViewBlog from './pages/blog/ViewBlog';
@@ -11,19 +10,12 @@ import Reports from './pages/blog/Reports';
 import AdminDashboard from './pages/Admin/adminDashboard';
 import ViewReport from './pages/blog/ViewReport';
 import CreateBlog from './features/blog/CreateBlog';
-
-// Uncomment and define UserContext if needed
-// export const UserContext = createContext({});
+import AdminLayout from './components/AdminLayout';
+import NewAdminPage from './pages/Admin/NewAdminpage';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
-  // const [userAuth, setUserAuth] = useState();
-  // useEffect(() => {
-  //   let userInSession = lookInSession("user");
-  //   userInSession ? setUserAuth(JSON.parse(userInSession)) : setUserAuth({ access_token: null });
-  // }, []);
-
   return (
-    //<UserContext.Provider value={{ userAuth, setUserAuth }}>
     <Routes>
       <Route element={<Wrapper />}>
         <Route path="/" element={<Home />} />
@@ -33,10 +25,17 @@ const App = () => {
         <Route path="blogs/report/:id" element={<ViewReport />} />
         <Route path="reports" element={<AdminDashboard />} />
       </Route>
+      <Route
+        path="/dashboard"
+        element={
+          <AdminLayout>
+            <Dashboard />
+          </AdminLayout>
+        }
+      />
       <Route path="blogs/edit/:id" element={<EditBlog />} />
       <Route path="/editor" element={<CreateBlog />} />
     </Routes>
-    //</UserContext.Provider>
   );
 };
 
